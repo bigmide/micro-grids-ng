75d0c6c5b9926c9067c69ca1c28c04b305efcd4b
Olumide Anthony, Ojo <67399095+bigmide@users.noreply.github.com>
2025-08-18 12:13:04 +0100

    integrated cloudflare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { reactRouter } from '@react-router/dev/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -6,10 +7,11 @@ import devtoolsJson from 'vite-plugin-devtools-json'
 import mdx from '@mdx-js/rollup'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-import rehypePrettyCode from 'rehype-pretty-code'
+import { rehypePrettyCode } from 'rehype-pretty-code'
 
 export default defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tsconfigPaths(),
     devtoolsJson(),
     tailwindcss(),
